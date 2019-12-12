229aacc0200e75a78656aa4cb2c1a11e3d68a00a
huijie-hu <Jessica.Hu@myob.com>
2019-12-12 16:22:02 +1100

    refactored task_reducer

--- todo_frontend/src/pages/reducers/task.ts
+++ todo_frontend/src/pages/reducers/task.ts
@@ -1,38 +1,46 @@
 import { combineReducers } from 'redux'
-import { Task, TaskActions, ITaskActionsRequest } from '../../models/task'
+import { Task, TaskActions, ITaskActionsRequest, TaskProgress } from '../../models/task'
 
 const todoTasks = (state: Task[] = [], action: ITaskActionsRequest): Task[] => {
   switch(action.type){
+
     case TaskActions.ADD_TASK:
     case TaskActions.UNDO_TASK:
+      if( !action.text ) break;
       const id = state.length===0 ? 1 : Math.random()
       return [...state,new Task(id, action.text)]
+
     case TaskActions.UPDATE_TASK:
+      if( !action.text ) break;
+      const task = action.text
       return state.map(t => {
-        if(t.id === action.id) t.task = action.text
+        if(t.id === action.id) t.task = task
         return t
       })
-    case TaskActions.COMPLETE_TASK:
-    case TaskActions.DELETE_TASK:
-      return state.filter(t => t.id !== action.id)
-  }
-  return state
-}
 
-const completedTasks = (state: Task[] = [], action: ITaskActionsRequest): Task[] => {
-  switch(action.type){
     case TaskActions.COMPLETE_TASK:
-      return [...state,new Task(action.id, action.text)]
+      if( !action.id ) break;
+      return state.map(t => {
+        if(t.id === action.id) t.progress = TaskProgress.DONE
+        return t
+      })
+
     case TaskActions.UNDO_TASK:
+      if( !action.id ) break;
+      return state.map(t => {
+        if(t.id === action.id) t.progress = TaskProgress.TODO
+        return t
+      })
+
     case TaskActions.DELETE_TASK:
       return state.filter(t => t.id !== action.id)
+
   }
   return state
 }
 
 const taskList = combineReducers({
-  todoTasks,
-  completedTasks
+  todoTasks
 })
 
 export default taskList
